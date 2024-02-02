587a14b911c60a31f2f91129e0593f2966668372
heyitsk21 <122894491+heyitsk21@users.noreply.github.com>
2024-02-02 15:32:15 -0600

    trying to fix local storage on new devices

--- functions.js
+++ functions.js
@@ -1,89 +1,36 @@
-// function loadStyle() {
-//     page_style = localStorage.getItem("page_stylesheet_name");
-//     if(page_style === null) {
-//         page_style = "second_style.css"; //this is what i'm making the default style
-//     }
-//     document.getElementById("page_style").setAttribute("href", page_style);
-// }
-
-// function switchCss() {
-//     if (page_style === "styles_home.css") {
-//         // document.getElementById("selected_css").setAttribute("href", "second_style.css");
-//         // page_style = "second_style.css";
-//         localStorage.setItem("page_stylesheet_name", "style_home.css");
-//     } else {
-//         // document.getElementById("selected_css").setAttribute("href", "style_home.css");
-//         // page_style = "style_home.css";
-//         localStorage.setItem("page_stylesheet_name", "second_style.css");
-//     }
-// }
+const STYLESHEET_KEY = "stylesheet";
+const DEFAULT = "style_home.css";
+const ALT = "second_style.css";
 
 function toggleStyleSheet() {
-    var element = document.getElementById("selected_css");
-    var stylename = element.getAttribute("href");
-    if (stylename == "style_home.css") {
-        stylename = "second_style.css";
+    var styleElement = document.getElementById("selected_css");
+    var fileName = styleElement.getAttribute("href");
+    var newFileName = DEFAULT;
+    if (fileName == ALT) {
+        newFileName = DEFAULT;
+    } else {
+        newFileName = ALT;
     }
-    else {
-        stylename = "style_home.css";
-    }
-
-    element.setAttribute("href", stylename);
-    localStorage.setItem("style", stylename);
+    styleElement.setAttribute("href", newFileName);
+    localStorage.setItem(STYLESHEET_KEY, newFileName);
 }
 
-window.onload = function() {
-    var local1 = localStorage.getItem("style");
-    var element = document.getElementById("selected_css");
-    var stylename = element.getAttribute("href");
-    element.setAttribute("href",local1);
-}
-
-// function setIcon(favImg){
-//     let headTitle = document.querySelector('head');
-//     let setFavicon = document.createElement('link');
-//     setFavicon.setAttribute('rel','icon');
-//     setFavicon.setAttribute('href',favImg);
-//     headTitle.appendChild(setFavicon);
-// }
+window.onload = function () {
+    var stylesheetValue = localStorage.getItem(STYLESHEET_KEY) || DEFAULT;
+    var styleElement = document.getElementById("selected_css");
+    styleElement.setAttribute("href", stylesheetValue);
+};
 
 function copyToClipboard(text) {
-    const tempInput = document.createElement('input');
+    const tempInput = document.createElement("input");
     tempInput.value = text;
     document.body.appendChild(tempInput);
     tempInput.select();
-    document.execCommand('copy');
+    document.execCommand("copy");
     document.body.removeChild(tempInput);
-    alert('Link copied to clipboard: ' + text);
+    alert("Link copied to clipboard: " + text);
 
     // var Text = document.getElementById("")
 
     // navigator.clipboard.writeText(text);
 }
-
-
-toggleStyleSheet()
-
-
-// loadStyle()
-
-
-// "1" + 1 = "11" //
-// "1" - 1 = 0    //
-
-
-
-// var num = 0; // global variable
-
-// function button1() {
-//     num++;
-//     document.getElementById("output").innerText = "Pressed x "+num+"of times.";
-// }
-
-// function switchToPlain() {
-//     document.getElementById("selected_css").setAttribute("href", "")
-// }
-
-// function switchToPlain() {
-//     document.getElementById("selected_css").setAttribute("href", "")
-// }
